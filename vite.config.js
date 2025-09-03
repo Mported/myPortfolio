bb3cf7c430a2c4b00d4d03ff483a36f310b22493
Miguel C <erqmac@gmail.com>
2025-09-03 16:51:27 -0700

    main branch fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Mported_dev/', // GitHub Pages repository name
+  base: '/myPortfolio/', // GitHub Pages repository name (repo: Mported/myPortfolio)
   plugins: [react({
     babel: {
       plugins: [['styled-jsx/babel', { optimizeForSpeed: true }]]
